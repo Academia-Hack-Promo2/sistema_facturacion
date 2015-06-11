29dc512c697fe42ce5987a0c05ddcd2f58fc4dba
Alex Ballera <alexballera@gmail.com>
2015-06-11 12:01:40 -0430

    Terminado las validaciones de las migraciones

--- back_facturacion/db/migrate/20150610163645_create_users.rb
+++ back_facturacion/db/migrate/20150610163645_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name, limit: 12
       t.string :last_name, limit: 12
       t.string :username, null: false, limit: 12
-      t.string :password, null: false, limit: 12
+      #t.string :password, null: false, limit: 12
 
       t.timestamps null: false
     end
--- back_facturacion/db/schema.rb
+++ back_facturacion/db/schema.rb
@@ -66,11 +66,10 @@ ActiveRecord::Schema.define(version: 20150610205822) do
     t.string   "first_name",             limit: 12
     t.string   "last_name",              limit: 12
     t.string   "username",               limit: 12,               null: false
-    t.string   "password",               limit: 12,               null: false
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 12,  default: "", null: false
+    t.string   "email",                  limit: 255, default: ""
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
