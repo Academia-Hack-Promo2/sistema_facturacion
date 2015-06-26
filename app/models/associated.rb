75179cae2bfc27e9d8e40a8ced9c6f01050a8d9e
Alex Ballera <alexballera@gmail.com>
2015-06-25 20:37:44 -0430

    Corregido Associated

--- app/models/associated.rb
+++ app/models/associated.rb
@@ -1,7 +1,7 @@
 class Associated < ActiveRecord::Base
   belongs_to :user
   has_many :invoices, dependent: :destroy
-  validates :name, :ci_rif, :alias,:email, :phone, :address, :description, :user_id, presence: true
+  validates :name, :ci_rif, :alias,:email, :phone, :address, :description, presence: true
   validates :kind, inclusion: { in: :kind }
   validates :status, inclusion: { in: :status }
   validates :frequency, inclusion: { in: :frequency }
